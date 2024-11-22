3dc11fa41056fe0e12e2d80a476c0493ac17d868
Dmitry <denq16@gmail.com>
2024-11-22 18:03:26 +0300

    Change end quiz message

--- messages.py
+++ messages.py
@@ -1,7 +1,7 @@
 START_MESSAGE = "привет"
 TRUE_ANSWER = "правильно"
 FALSE_ANSWER = "неправильно."
-END_QUIZ = "закончил опрос"
+END_QUIZ = "Поздравляю, вы прошли опрос! Надеюсь, вы узнали для себя что-то новое про безопасность."
 #FALSE_ANSWER = "неправильно. {description}"
 #
 #print(FALSE_ANSWER.format(description="Описание"))
